201f9ca7ae190beba03e3a9d1f2895372f76ea8a
Thigoo <thiagomvk08@gmail.com>
2026-08-02 07:40:01 -0300

    fix: propriedade user para conexão do banco

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const poolConfig: PoolOptions = {
   host: process.env.DB_HOST || "",
   port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER || "",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "",
   waitForConnections: true,
